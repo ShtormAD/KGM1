b67f30a170b36fa4d6c0635e8acc55890e13d50f
bkmzb <bkmzb@TUF_AD>
2022-05-11 13:01:34 +0700

    Добавьте файлы проекта.

---
+++ KGM1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace KGM1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPnt = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnRect = new System.Windows.Forms.Button();
+            this.btnSqr = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.btnClr = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPnt
+            // 
+            this.btnPnt.Font = new System.Drawing.Font("Franklin Gothic Demi", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPnt.Location = new System.Drawing.Point(410, 5);
+            this.btnPnt.Name = "btnPnt";
+            this.btnPnt.Size = new System.Drawing.Size(120, 30);
+            this.btnPnt.TabIndex = 0;
+            this.btnPnt.Text = "Точка\r\n";
+            this.btnPnt.UseVisualStyleBackColor = true;
+            this.btnPnt.Click += new System.EventHandler(this.btnPnt_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Font = new System.Drawing.Font("Franklin Gothic Demi", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLine.Location = new System.Drawing.Point(410, 40);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(120, 30);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.Text = "Линия";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnRect
+            // 
+            this.btnRect.Font = new System.Drawing.Font("Franklin Gothic Demi", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRect.Location = new System.Drawing.Point(410, 75);
+            this.btnRect.Name = "btnRect";
+            this.btnRect.Size = new System.Drawing.Size(120, 30);
+            this.btnRect.TabIndex = 2;
+            this.btnRect.Text = "Прямоугольник";
+            this.btnRect.UseVisualStyleBackColor = true;
+            this.btnRect.Click += new System.EventHandler(this.btnRect_Click);
+            // 
+            // btnSqr
+            // 
+            this.btnSqr.Font = new System.Drawing.Font("Franklin Gothic Demi", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSqr.Location = new System.Drawing.Point(410, 110);
+            this.btnSqr.Name = "btnSqr";
+            this.btnSqr.Size = new System.Drawing.Size(120, 30);
+            this.btnSqr.TabIndex = 3;
+            this.btnSqr.Text = "Квадрат";
+            this.btnSqr.UseVisualStyleBackColor = true;
+            this.btnSqr.Click += new System.EventHandler(this.btnSqr_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Font = new System.Drawing.Font("Franklin Gothic Demi", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCircle.Location = new System.Drawing.Point(410, 145);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(120, 30);
+            this.btnCircle.TabIndex = 4;
+            this.btnCircle.Text = "Круг";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.White;
+            this.pb1.Location = new System.Drawing.Point(5, 5);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(400, 400);
+            this.pb1.TabIndex = 5;
+            this.pb1.TabStop = false;
+            // 
+            // btnClr
+            // 
+            this.btnClr.Font = new System.Drawing.Font("Franklin Gothic Demi", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClr.Location = new System.Drawing.Point(410, 180);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(120, 30);
+            this.btnClr.TabIndex = 6;
+            this.btnClr.Text = "Очистить";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 411);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnSqr);
+            this.Controls.Add(this.btnRect);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.btnPnt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Примитивы";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPnt;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnRect;
+        private System.Windows.Forms.Button btnSqr;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Button btnClr;
+    }
+}
+
---
+++ KGM1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KGM1
+{
+    public partial class Form1 : Form
+    {
+        static private Graphics g;
+        static private Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pb1.CreateGraphics();
+            rnd = new Random();
+        }
+
+        private void btnPnt_Click(object sender, EventArgs e)
+        {
+            SolidBrush sb = new SolidBrush(getRndCol());
+            g.FillEllipse(sb, new Rectangle( 10+ rnd.Next(380), 10 + rnd.Next(380), 3,3));
+            //g.DrawEllipse(myPen, new Rectangle(0, 0, 200, 300));
+            sb.Dispose();
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            Pen myPen = new Pen(getRndCol());
+            g.DrawLine(myPen, 10 + rnd.Next(380), 10 + rnd.Next(380), 10 + rnd.Next(380), 10 + rnd.Next(380));
+            myPen.Dispose();
+        }
+
+        private void btnRect_Click(object sender, EventArgs e)
+        {
+            SolidBrush sb = new SolidBrush(getRndCol());
+            g.FillRectangle(sb, new Rectangle(10 + rnd.Next(220), 10 + rnd.Next(220), rnd.Next(200), rnd.Next(200)));
+            sb.Dispose();
+        }
+
+        private void btnSqr_Click(object sender, EventArgs e)
+        {
+            int a = rnd.Next(200);
+            SolidBrush sb = new SolidBrush(getRndCol());
+            g.FillRectangle(sb, new Rectangle(10 + rnd.Next(220), 10 + rnd.Next(220), a, a ));
+            sb.Dispose();
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            int a = rnd.Next(200);
+            SolidBrush sb = new SolidBrush(getRndCol());
+            g.FillEllipse(sb, new Rectangle(10 + rnd.Next(220), 10 + rnd.Next(220), a, a));
+            sb.Dispose();
+        }
+
+        private Color getRndCol()
+        {
+            return Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+        }
+    }
+}
